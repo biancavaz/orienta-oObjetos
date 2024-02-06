de8a9435738dd419d79d8eb9c6b3c95f9a45320a
biancavaz <147559714+biancavaz@users.noreply.github.com>
2024-02-05 21:57:54 -0300

    05.02

---
+++ orientaçãoObjetos/src/escolaTech/Aluno.java
@@ -0,0 +1,36 @@
+package escolaTech;
+
+public class Aluno {
+	
+	// atributos :
+	String nome; 
+	String senha;
+	String email; 
+	String serie;
+	// métodos (funções):
+	
+	public boolean verificaSenha(String senha) {
+		if (senha.length()>=6) {
+			if (senha.matches(".*[0-9].*")&&senha.matches(".*[a-z].*")&&senha.matches(".*[A-Z].*")) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean verificaEmail(String email) {
+		if (email.contains("@"))
+			this.email = email;
+		else
+			return false;
+		return true; 
+	}
+	
+	public String mostrarDados() {
+		String dados = "\n Nome: " + nome + "\n e-mail: " + email; 
+				
+		return dados; 
+	}
+	
+
+}
---
+++ orientaçãoObjetos/src/escolaTech/principal.java
@@ -0,0 +1,32 @@
+package escolaTech;
+
+import java.util.Scanner;
+
+public class principal {
+	
+	public static void main (String [] args) {
+		System.out.println(" informe nome do aluno: ");
+		Scanner teclado = new Scanner ( System.in);
+		// criar um objeto da classe aluno:
+		Aluno aluno1= new Aluno();
+		// atribuindo dado ao noem:
+		aluno1.nome = teclado.nextLine();
+		
+		System.out.println("informe a senha: ");
+		if(aluno1.verificaSenha (teclado.nextLine())) {
+		do {
+			System.out.println(" informe a senha novamente : ");
+		} while (aluno1.verificaSenha(teclado.nextLine()));
+		}
+		System.out.println(" informe o e-mail: ");
+		aluno1.verificaEmail (teclado.nextLine());
+		// perguntar email de novo se for inválido
+		do {
+			System.out.println(" informe o email novamente : ");
+			
+		} while (!aluno1.verificaEmail( teclado.nextLine()));
+		System.out.println(aluno1.mostrarDados());
+		
+	}
+	
+}
